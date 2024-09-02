1a72ee15802f329362aa0724f50a34b69384f4f4
Scytheez <floresvittorio@gmail.com>
2024-09-02 20:23:58 +0800

    update

---
+++ main_parasitized.py
@@ -0,0 +1,180 @@
+import os
+import pathlib
+import numpy as np
+import tensorflow as tf
+import matplotlib.pyplot as plt
+
+import seaborn as sns
+sns.set_style('darkgrid')
+from sklearn.metrics import confusion_matrix, classification_report
+
+from keras.models import load_model
+
+img_height = 180
+img_width = 180
+batch_size = 32 
+
+val_count = []
+inval_count = []
+
+def validate():
+    try:
+        dataset_path = '../../Documents/Malaria Dataset/validation dataset' # ChangeMe
+        data_dir = pathlib.Path(dataset_path).with_suffix('')
+
+        train_ds = tf.keras.utils.image_dataset_from_directory(
+            data_dir,
+            validation_split=0.2,
+            subset="training",
+            seed=123,
+            image_size=(img_height, img_width),
+            batch_size=batch_size)
+        
+        class_names = train_ds.class_names
+
+        val_model_path = 'trained_model/val_model.keras'
+        model = load_model(val_model_path)
+
+        model.summary()
+
+        input_dir = 'parasitized_cell'
+
+        print('VALIDATE IMAGE')
+        for filename in os.listdir(input_dir):
+            if filename.lower().endswith((".png", ".jpg", ".jpeg")):
+                img_path = os.path.join(input_dir, filename)
+                img = tf.keras.utils.load_img(img_path, target_size=(img_height, img_width))
+                img_array = tf.keras.utils.img_to_array(img)
+                img_array = tf.expand_dims(img_array, 0)
+
+                predictions = model.predict(img_array)
+                score = tf.nn.softmax(predictions[0])
+                confidence = ("{:.2f}".format(100 * np.max(score)))
+
+                print("Image: ", filename)
+                print(f'{class_names[np.argmax(score)]} = {score}')
+                print(f"Confidence Level: {confidence}")
+                print("------------------------------------")
+
+                predicted_class = class_names[np.argmax(score)]
+
+                if predicted_class == 'invalid':
+                    inval_count.append(filename)
+                elif predicted_class == 'valid':
+                    val_count.append(filename)
+                else:
+                    print('Something went wrong')
+
+    except Exception as e:
+        print(e)
+
+    finally:
+        print()
+        print(f'Valid: {len(val_count)}')
+        print(f'Invalid: {len(inval_count)}')
+        print()
+        print('===============================================================')
+        print()
+        print()
+
+
+def main():
+    try:
+        # Dataset path
+        dataset_path = '../../Documents/Malaria Dataset/Dataset Pixel' # Changeme
+        data_dir = pathlib.Path(dataset_path).with_suffix('')
+
+        img_height = 180
+        img_width = 180
+        batch_size = 32
+
+        train_ds = tf.keras.utils.image_dataset_from_directory(
+            data_dir,
+            validation_split=0.2,
+            subset="training",
+            seed=123,
+            image_size=(img_height, img_width),
+            batch_size=batch_size)
+        class_names = train_ds.class_names
+
+        # User input image
+        input_dir = "parasitized_cell"
+
+        # Saved model path
+        model_path = "trained_model/my_model.keras"
+
+        model = load_model(model_path)
+        model.summary()
+
+        un_count = []
+        par_count = []
+        predicted = []
+
+        for filename in os.listdir(input_dir):
+            if filename.endswith(".png") or filename.endswith(".jpg"):
+                img_path = os.path.join(input_dir, filename)
+                img = tf.keras.utils.load_img(img_path, target_size=(img_height, img_width))
+                img_array = tf.keras.utils.img_to_array(img)
+                img_array = tf.expand_dims(img_array, 0)
+
+                predictions = model.predict(img_array)
+                score = tf.nn.softmax(predictions[0])
+
+                predicted_class = class_names[np.argmax(score)]
+
+                print("Image: ", filename)
+                if predicted_class == 'Uninfected':
+                    print('Prediction: Uninfected Cell')
+                    print("Confidence Level: {:.2f}%".format(100 * np.max(score)))
+                    un_count.append(filename)
+                    predicted.append(predicted_class)
+                    print("------------------------------------")
+                elif predicted_class == 'Parasitized':
+                    print('Prediction: Infected Cell')
+                    print("Confidence Level: {:.2f}%".format(100 * np.max(score)))
+                    par_count.append(filename)
+                    predicted.append(predicted_class)
+                    print("------------------------------------")
+                else:
+                    print('Not a type of sample!')
+                    
+    except Exception as e:
+        print(e)
+
+    print(f'Uninfected: {len(un_count)}')
+    print(f'Parasitized: {len(par_count)}')
+    #print(predicted, len(predicted))
+    print('------------------------------------------------------')
+
+    # CONFUSION MATRIX
+    expected = []
+
+    for i in range(100):
+        expected.append('Parasitized')
+
+    expected = np.array(expected)
+    predicted = np.array(predicted)
+
+    print(f'{expected} :: {len(expected)} \n {predicted} :: {len(predicted)}')
+
+    cm = confusion_matrix(expected, predicted, labels=['Parasitized', 'Uninfected'])
+    print(f'Confusion Matrix Value: {cm} | {cm.shape}')
+
+    plt.figure(figsize=(8, 8))
+    sns.heatmap(cm, annot=True, fmt='g', cmap='Blues', 
+                xticklabels=['Parasitized', 'Uninfected'], 
+                yticklabels=['Parasitized', 'Uninfected'])
+    plt.title('Confusion Matrix')
+    plt.ylabel('Expected')
+    plt.xlabel('Predicted')
+    plt.gca().xaxis.set_label_position('top')
+    plt.gca().xaxis.tick_top()
+    plt.gca().figure.subplots_adjust(bottom=0.2)
+
+    print(classification_report(expected, predicted))
+
+    plt.show()
+
+if __name__ == '__main__':
+    validate()
+    main()
--- main_uninfected.py
+++ main_uninfected.py
@@ -3,13 +3,14 @@ import pathlib
 import numpy as np
 import tensorflow as tf
 import matplotlib.pyplot as plt
-
+import seaborn as sns
+sns.set_style('darkgrid')
+from sklearn.metrics import confusion_matrix, classification_report
 from keras.models import load_model
 
 img_height = 180
 img_width = 180
 batch_size = 32 
-
 val_count = []
 inval_count = []
 
@@ -31,11 +32,9 @@ def validate():
         val_model_path = 'trained_model/val_model.keras' # Changeme
         model = load_model(val_model_path)
 
-        #model.evaluate(train_ds)
         model.summary() 
 
-        #input_dir = 'parasitized_cell'
-        input_dir = 'uninfected_cell' # Changme
+        input_dir = 'uninfected_cell'
 
         print('VALIDATE IMAGE')
         for filename in os.listdir(input_dir):
@@ -79,7 +78,7 @@ def validate():
 def main():
     try:
         # Dataset path
-        dataset_path = '../../Documents/Malaria Dataset/Dataset Pixel'
+        dataset_path = '../../Documents/Malaria Dataset/Dataset Pixel' # Changeme
         data_dir = pathlib.Path(dataset_path).with_suffix('')
 
         img_height = 180
@@ -96,18 +95,17 @@ def main():
         class_names = train_ds.class_names
 
         # User input image
-        input_dir = "parasitized_cell"
-        #input_dir = "uninfected_cell"
+        input_dir = "uninfected_cell"
+
         # Saved model path
         model_path = "trained_model/my_model.keras"
 
         model = load_model(model_path)
-        #model.evaluate(train_ds)
         model.summary()
 
         un_count = []
         par_count = []
-        overall = []
+        predicted = []
 
         for filename in os.listdir(input_dir):
             if filename.endswith(".png") or filename.endswith(".jpg"):
@@ -126,63 +124,51 @@ def main():
                     print('Prediction: Uninfected Cell')
                     print("Confidence Level: {:.2f}%".format(100 * np.max(score)))
                     un_count.append(filename)
-                    overall.append(predicted_class)
-                    #np.append(overall, [predicted_class])
+                    predicted.append(predicted_class)
                     print("------------------------------------")
                 elif predicted_class == 'Parasitized':
                     print('Prediction: Infected Cell')
                     print("Confidence Level: {:.2f}%".format(100 * np.max(score)))
                     par_count.append(filename)
-                    overall.append(predicted_class)
-                    #np.append(overall, [predicted_class])
+                    predicted.append(predicted_class)
                     print("------------------------------------")
                 else:
                     print('Not a type of sample!')
+                    
     except Exception as e:
         print(e)
 
     print(f'Uninfected: {len(un_count)}')
     print(f'Parasitized: {len(par_count)}')
-    #print(overall, len(overall))
+    #print(predicted, len(predicted))
     print('------------------------------------------------------')
 
     # CONFUSION MATRIX
-    import seaborn as sns
-    sns.set_style('darkgrid')
-    from sklearn.datasets import make_classification
-    from sklearn.model_selection import train_test_split
-    from sklearn.metrics import confusion_matrix, classification_report
-    from sklearn.ensemble import RandomForestClassifier
+    expected = []
 
-    X, y = make_classification(n_samples=333, n_features=20, n_classes=2, random_state=42)
+    for i in range(100):
+        expected.append('Uninfected')
 
-    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=42)
+    expected = np.array(expected)
+    predicted = np.array(predicted)
 
-    model = RandomForestClassifier(random_state=42)
-    model.fit(X_train, y_train)
+    print(f'{expected} :: {len(expected)} \n {predicted} :: {len(predicted)}')
 
-    y_pred = model.predict(X_test)
-
-    #y_test = np.array([])
-    y_test = []
-    for i in range(0,100):
-        #y_test.append(['Uninfected'])
-        y_test.append(['Parasitized'])
-    #print(xy_test, len(xy_test))
-
-    cm = confusion_matrix(y_test, overall, labels=['Parasitized', 'Uninfected']) #y_test | y_pred
+    cm = confusion_matrix(expected, predicted, labels=['Uninfected', 'Parasitized'])
     print(f'Confusion Matrix Value: {cm} | {cm.shape}')
 
     plt.figure(figsize=(8, 8))
-    sns.heatmap(cm, annot=True, fmt='g', cmap='Blues', xticklabels=['Parasitized', 'Uninfected'], yticklabels=['Parasitized', 'Uninfected'])
+    sns.heatmap(cm, annot=True, fmt='g', cmap='Blues', 
+                xticklabels=['Uninfected', 'Parasitized'], 
+                yticklabels=['Uninfected', 'Parasitized'])
     plt.title('Confusion Matrix')
-    plt.ylabel('True label')
-    plt.xlabel('Predicted label')
+    plt.ylabel('Expected')
+    plt.xlabel('Predicted')
     plt.gca().xaxis.set_label_position('top')
     plt.gca().xaxis.tick_top()
     plt.gca().figure.subplots_adjust(bottom=0.2)
 
-    print(classification_report(y_test, overall)) #y_test | y_pred | , target_names=class_names
+    print(classification_report(expected, predicted))
 
     plt.show()
 
